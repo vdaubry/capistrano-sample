826d174da34d348c6d09e019ddc447f5fadc364e
vincent daubry <vdaubry@gmail.com>
2014-03-30 06:54:52 +0200

    remplace libV8 par nodejs

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :deploy_to, '/srv/www/capistrano-sample'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 1
 
 namespace :deploy do
 
